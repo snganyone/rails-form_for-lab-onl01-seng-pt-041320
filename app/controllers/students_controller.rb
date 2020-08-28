aecf5441d0a2abcff2072fed16b1a53b3a46e12f
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-08-27 20:44:46 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,6 @@
 class StudentsController < ApplicationController
     def index
-        @student = Student.all
+        @students = Student.all
     end
 
     def new
@@ -12,15 +12,19 @@ class StudentsController < ApplicationController
         @student.save
         redirect_to student_path(@student)
     end
+    
+    def update
+        @student = Student.find(params[:id])
+        @student.update(params.require(:student).permit(:first_name, :last_name))
+        redirect_to student_path(@student)
+    end
 
     def show
         @student = Student.find(params[:id])
     end
 
     def edit
-    end
-
-    def update
+        @student = Student.find(params[:id])
     end
 
     private
