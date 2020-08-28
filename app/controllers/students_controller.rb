c40df02514735d2c1c03e39dbbd851e60b6a9c9d
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-08-27 19:52:48 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,7 +8,9 @@ class StudentsController < ApplicationController
     end
 
     def create
-        @student = Student.new()
+        @student = Student.new(params.require(:student).permit(:first_name, :last_name))
+        @student.save
+        redirect_to student_path(@student)
     end
 
     def show
